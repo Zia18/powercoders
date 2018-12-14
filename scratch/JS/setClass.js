57f3fca2941188633f8ed7bed2f7b621f6c20a20
Zia Ensafi <zia.ensafi@powercoders.org>
2018-12-14 16:39:53 +0100

    My last js file update

--- scratch/JS/setClass.js
+++ scratch/JS/setClass.js
@@ -5,14 +5,7 @@ function greet(name) {
   console.log('hello, ' + name);
  }
 
-  function setClassTip(event) {
-   let el = document.getElementById('mypara').className = 'tip';
- }
-
- function setClassWarning(event) {
-  let el = document.getElementById('mypara').className = 'warning';
- }
-
- function clearClass(event) {
-  let el = document.getElementById('mypara').className = '';
- }
+function setClass(event, className) {
+     let el = document.getElementById('mypara');
+     el.className = className;
+}
