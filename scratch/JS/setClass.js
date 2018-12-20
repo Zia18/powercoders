6f7b4fd21dc18e02e6e4a1f29161cbadb775d5e2
Zia Ensafi <zia.ensafi@powercoders.org>
2018-12-20 13:48:12 +0100

    JS last update

--- scratch/JS/setClass.js
+++ scratch/JS/setClass.js
@@ -5,7 +5,23 @@ function greet(name) {
   console.log('hello, ' + name);
  }
 
-function setClass(event, className) {
+function setClass(className) {
      let el = document.getElementById('mypara');
      el.className = className;
 }
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('make-warning').addEventListener('click', function (event) {
+        setClass('warning');
+  });
+
+    document.getElementById('make-tip').addEventListener('click', function (event) {
+      setClass('tip');
+  });
+    
+    document.getElementById('make-normal').addEventListener('click', function (event) {
+      setClass('');
+  });
+});
+
