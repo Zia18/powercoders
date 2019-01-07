50c94ebc72f102514f0568d82ab6ae1ca61625f6
Zia Ensafi <zia.ensafi@powercoders.org>
2019-01-07 14:46:02 +0100

    Shopping list updated.

---
+++ shopping/shopping.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.querySelector('button').addEventListener('click', function (event) {
+        let inputBox = document.getElementById('item');
+        console.log(inputBox.value);
+    });
+});
