20188ef5af9364e5d4aaaa704b5e9e4584fe3d43
Zia Ensafi <zia.ensafi@powercoders.org>
2019-01-10 14:38:25 +0100

    Allow user to add list item by pressing "Enter"

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,6 +1,34 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        let inputBox = document.getElementById('item');
-        console.log(inputBox.value);
-    });
+  document.querySelector('button').addEventListener('click', function (event) {
+    let inputBox = document.getElementById('item');
+    let li = createNewListItem(inputBox.value);
+    document.querySelector('ul').appendChild(li);
+  });
 });
+
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    let inputBox = document.getElementById('item');
+    let li = createNewListItem(inputBox.value);
+    document.querySelector('ul').appendChild(li);
+  }
+});
+
+function createNewListItem(itemText) {
+  console.log(itemText);
+  let li = document.createElement('li');
+  let span = document.createElement('span');
+  let spanText = document.createTextNode(itemText);
+  span.appendChild(spanText);
+  li.appendChild(span);
+
+  let button = document.createElement('button');
+  let text = document.createTextNode('delete');
+  button.addEventListener('click', function (event) {
+    li.remove();
+  });
+
+  button.appendChild(text);
+  li.appendChild(button);
+  return li;
+}
