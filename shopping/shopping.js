aea8165088f1ba175526568d57f007d2e6878dff
Zia Ensafi <zia.ensafi@powercoders.org>
2019-01-11 16:20:46 +0100

    let replaced with const!

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  let inputBox = document.getElementById('item');
-  let shoppingList = document.querySelector('ul');
-  let button = document.querySelector('button');
+  const inputBox = document.getElementById('item');
+  const shoppingList = document.querySelector('ul');
+  const button = document.querySelector('button');
 
   document.querySelector('button').addEventListener('click', function (event) {
     if (inputBox.value.trim() !== '') {
@@ -31,15 +31,15 @@ document.addEventListener('DOMContentLoaded', function (event) {
 });
 
 function createNewListItem(itemText) {
-  let li = document.createElement('li');
-  let span = document.createElement('span');
-  let spanText = document.createTextNode(itemText);
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const spanText = document.createTextNode(itemText);
   span.appendChild(spanText);
   li.appendChild(span);
 
-  let button = document.createElement('button');
+  const button = document.createElement('button');
   document.getElementById('item').focus();
-  let text = document.createTextNode('delete');
+  const text = document.createTextNode('delete');
   button.addEventListener('click', function (event) {
     li.remove();
     document.getElementById('item').focus();
