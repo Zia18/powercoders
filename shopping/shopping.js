723b0e715c3350bc16d5018a169924ced8f1faf4
Zia Ensafi <zia.ensafi@powercoders.org>
2019-01-15 14:42:10 +0100

    Added some new function to shopping list.

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,34 +1,46 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   const inputBox = document.getElementById('item');
   const shoppingList = document.querySelector('ul');
-  const button = document.querySelector('button');
+  const addItemButton = document.querySelector('button');
 
-  document.querySelector('button').addEventListener('click', function (event) {
-    if (inputBox.value.trim() !== '') {
-      button.disabled = true;
-      shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
-      inputBox.value = '';
+  addItemButton.addEventListener('click', function (event) {
+    const trimmedValue = inputBox.value.trim();
 
-    }
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
+    addItemButton.disabled = true;
     inputBox.focus();
   });
 
   inputBox.addEventListener('keyup', function (event) {
-    if (inputBox.value.trim() !== '') {
-      button.disabled = false;
-      if (event.key === 'Enter') {
-        shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
-        inputBox.value = '';
-      }
-    }
-    if (inputBox.value.trim() === '') {
-      button.disabled = true;
-    }
-  });
-  button.disabled = true;
-  inputBox.focus();
+    const trimmedValue = inputBox.value.trim();
+    addItemButton.disabled = inputBox.value.trim() === '';
 
+    if (trimmedValue === '') {
+      return;
+    }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
+    addItemButton.disabled = true;
+  });
+
+  addItemButton.disabled = true;
+  inputBox.focus();
 });
+
+/**
+ * Creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * @param {string} itemText Name of the item to add to the list
+ * @return {HTMLElement} li element
+ */
 
 function createNewListItem(itemText) {
   const li = document.createElement('li');
@@ -37,15 +49,14 @@ function createNewListItem(itemText) {
   span.appendChild(spanText);
   li.appendChild(span);
 
-  const button = document.createElement('button');
+  const icon = document.createElement('i');
+  li.appendChild(icon).className = 'fas fa-trash';
   document.getElementById('item').focus();
-  const text = document.createTextNode('delete');
-  button.addEventListener('click', function (event) {
+  icon.addEventListener('click', function (event) {
     li.remove();
     document.getElementById('item').focus();
   });
-
-  button.appendChild(text);
-  li.appendChild(button);
   return li;
 }
+
+
