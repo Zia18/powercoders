19a09a1517b75f9bd997056cad267913f29324f2
Zia Ensafi <zia.ensafi@powercoders.org>
2019-02-05 14:03:11 +0100

    Updated View from Model!

--- shopping-mvc/src/js/model.js
+++ shopping-mvc/src/js/model.js
@@ -4,14 +4,26 @@
  * The list is modelled as an array.
  */
 class Model {
+
   /** @param controller {!Controller} App controller*/
   constructor(controller) {
     console.log('Model initialised');
 
     /** @private {!ShoppingListItem[]} Item in the list */
-    this.item_ = [];
+    this.items_ = [
+      new ShoppingListItem('First item', 'q1'),
+      new ShoppingListItem('Second item', 'q2'),
+      new ShoppingListItem('Third item', 'q3')
+    ];
 
     /** @private {!View} View for this model. */
     this.view_ = new View(this, controller);
+
+    this.view_.update();
   }
+
+  get items() {
+    return this.items_.slice();
+  }
+
 }
